eb646a24e5c255b6bfa2a9a0b08327b5d06744bb
Hugh Perkins <hughperkins@gmail.com>
2016-08-08 04:33:33 +0100

    dont close new files

--- closeondelete.py
+++ closeondelete.py
@@ -7,6 +7,8 @@ class MyEvents(sublime_plugin.EventListener):
     def on_activated(self, view):
         if view.is_dirty():
             return
+        if view.command_history(0)[0] == '':  # means we just opened new file
+            return
         filename = view.file_name()
         if filename is not None and not os.path.exists(filename):
             view.set_scratch(True)
